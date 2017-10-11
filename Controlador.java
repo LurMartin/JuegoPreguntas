8b48ea07d931d42e625f0beb64bea3ebb958d80f
LurMartin <32668925+LurMartin@users.noreply.github.com>
2017-10-11 10:22:10 +0200

    Add files via upload

--- Controlador.java
+++ Controlador.java
@@ -5,10 +5,36 @@
  */
 package juegopalabras;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  *
  * @author Lourdes
  */
-public class Controlador {
+public class Controlador implements KeyListener{
+    private Vista vista;
+    private Modelo modelo;
+    
+    public Controlador (Vista vista){
+        this.vista=vista;
+        vista.addKeyListener(this);
+        modelo=new Modelo();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- Vista.java
+++ Vista.java
@@ -5,10 +5,55 @@
  */
 package juegopalabras;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Lourdes
  */
-public class Vista {
+public class Vista extends JFrame{
+    private String pregunta;
+    private String opcionesRes;
+    private String respuestaAcertada;
+    private String contadorAciertos;
     
+    public Vista(){
+        this.setVisible(true);
+        this.setBounds(200,200,1200,720);
+    }
+    
+    @Override
+    public void paint(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect(0,0,1200,720);
+        Font font=new Font("Harlow-solid",Font.BOLD,56);
+        g.setFont(font);
+        g.setColor(Color.BLUE);
+        g.drawString("Juego de Preguntas", 380,90);
+         //Resultado
+        g.setColor(Color.BLACK);
+        g.drawString("Pregunta: ",50, 350);
+        //Palabra
+        g.setColor(Color.BLACK);
+        g.drawString(pregunta, 400, 300);
+       //respuesta correcta
+        g.setColor(Color.BLUE);
+        g.drawString(opcionesRes, 400, 350);
+        //contador de aciertos
+        g.setColor(Color.RED);
+        g.drawString(contadorAciertos, 700, 120);
+    }
+    
+    public void setPregunta (String pregunta){
+        this.pregunta=pregunta;
+    }
+    public void setOpciones (String opcionesRes){
+        this.opcionesRes=opcionesRes;
+    }
+    public void setContadorAciertos (String contadorAciertos){
+        this.contadorAciertos=contadorAciertos;
+    }
 }
