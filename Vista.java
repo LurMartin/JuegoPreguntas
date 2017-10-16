f08b30c0fc7c1acbf9c9f9dce2b5a67c2875067b
LurMartin <32668925+LurMartin@users.noreply.github.com>
2017-10-16 11:51:12 +0200

    Update Vista.java

--- Vista.java
+++ Vista.java
@@ -16,9 +16,12 @@ import javax.swing.JFrame;
  */
 public class Vista extends JFrame{
     private String pregunta;
-    private String opcionesRes;
+    private String opcionA;
+    private String opcionB;
+    private String opcionC;
     private String respuestaAcertada;
-    private String contadorAciertos;
+    private String respuestaErronea;
+    private String contadorAciertos="0";
     
     public Vista(){
         this.setVisible(true);
@@ -29,31 +32,75 @@ public class Vista extends JFrame{
     public void paint(Graphics g){
         g.setColor(Color.white);
         g.fillRect(0,0,1200,720);
-        Font font=new Font("Harlow-solid",Font.BOLD,56);
+        Font font=new Font("Harlow-solid",Font.BOLD,30);
         g.setFont(font);
-        g.setColor(Color.BLUE);
-        g.drawString("Juego de Preguntas", 380,90);
+        g.setColor(Color.LIGHT_GRAY);
+        g.drawString("Juego de Preguntas", 380,80);
          //Resultado
         g.setColor(Color.BLACK);
-        g.drawString("Pregunta: ",50, 350);
+        g.drawString("Pregunta: ",20, 220);
         //Palabra
         g.setColor(Color.BLACK);
-        g.drawString(pregunta, 400, 300);
-       //respuesta correcta
+        g.drawString(pregunta, 20, 280);
+       //Opciones de respuesta
         g.setColor(Color.BLUE);
-        g.drawString(opcionesRes, 400, 350);
+        g.drawString(opcionA, 50, 370);
+        g.setColor(Color.BLUE);
+        g.drawString(opcionB, 50, 440);
+        g.setColor(Color.BLUE);
+        g.drawString(opcionC, 50, 500);
         //contador de aciertos
-        g.setColor(Color.RED);
-        g.drawString(contadorAciertos, 700, 120);
+        g.setColor(Color.orange);
+        g.drawString("Aciertos: ",800, 120);
+        g.setColor(Color.orange);
+        g.drawString(contadorAciertos, 1000, 120);
+        //Resaltar pregunta acertada o fallada
+        if (respuestaAcertada.equals("opcionA")){
+            g.setColor(Color.green);
+            g.drawString(opcionA, 50, 370);
+            g.drawString("¡Respuesta correcta!", 380, 600);
+        }
+        if (respuestaAcertada.equals("opcionB")){
+            g.setColor(Color.green);
+            g.drawString(opcionB, 50, 440);
+            g.drawString("¡Respuesta correcta!", 380, 600);
+        }
+        if (respuestaAcertada.equals("opcionC")){
+            g.setColor(Color.green);
+            g.drawString(opcionC, 50, 500);
+            g.drawString("¡Respuesta correcta!", 380, 600);
+        }
+        if(respuestaErronea.equals("opcionIncorrecta")){
+            g.setColor(Color.red);
+            g.drawString("Respuesta incorrecta", 380, 600);
+            
+        }
     }
+    
     
     public void setPregunta (String pregunta){
         this.pregunta=pregunta;
     }
-    public void setOpciones (String opcionesRes){
-        this.opcionesRes=opcionesRes;
+    public void setOpcionA (String opcionA){
+        this.opcionA=opcionA;
     }
-    public void setContadorAciertos (String contadorAciertos){
+    public void setOpcionB (String opcionB){
+        this.opcionB=opcionB;
+    }
+    public void setOpcionC (String opcionC){
+        this.opcionC=opcionC;
+    }
+    public void setSolucion(String solucion){
+        if (solucion.equals("opcionIncorrecta")){
+            respuestaErronea=solucion;
+        }
+        else{
+            respuestaAcertada=solucion;
+        }
+        
+    }
+   
+    public void setContador (String contadorAciertos){
         this.contadorAciertos=contadorAciertos;
     }
 }
